6b7c95781fadc01eb3a7b57f3d5f9068f1a8083b
pawel717 <psuchanicz@gmail.com>
2019-10-14 02:01:57 +0200

    Knn train and predict metrics
    - measure time of knn training and prediction
    - use MnistFileManager to read dataset from file

--- python/prir/app.py
+++ python/prir/app.py
@@ -1,18 +1,32 @@
 from prir.config import app_logging
-from prir.machinelearning import dataset
+from prir.files.mnist_file_manager import MnistFileManager
 from prir.metrics import timer
 import prir.machinelearning.standarization.standarizator as standarizator
+import prir.machinelearning.classifier.knn as knn
+
 
 logger = app_logging.get_logger(__name__)
 logger.debug("APPLICATION START")
 
-train_data = dataset.read_test_data()
-standarized_min_max = timer.call_and_measure("""standarizator.min_max_scaler(train_data)""", number=2)
-standarized_standard_scaler = timer.call_and_measure("""standarizator.standard_scaler(train_data)""", number=2)
+dataset = MnistFileManager().read_dataset()
+
+train_data_standarized_min_max = timer.call_and_measure(
+        """standarizator.min_max_scaler(dataset.get('train_data'))""",
+        number=10)
+timer.call_and_measure(
+        """knn.train_predict(train_data_standarized_min_max, dataset.get('train_target'), 
+        dataset.get('test_data'), dataset.get('test_target'))""",
+        number=10)
+del train_data_standarized_min_max
+
+
+train_data_standarized_standard_scaler = timer.call_and_measure(
+    """standarizator.standard_scaler(dataset.get('train_data'))""",
+    number=1)
+timer.call_and_measure(
+        """knn.train_predict(train_data_standarized_standard_scaler, dataset.get('train_target'), 
+        dataset.get('test_data'), dataset.get('test_target'))""",
+        number=1)
+del train_data_standarized_standard_scaler
 
 logger.debug("APPLICATION END")
-
-
-
-
-
