93e4df6bf756a6251426685d2c476832a930bf88
pawel717 <psuchanicz@gmail.com>
2019-10-14 23:37:32 +0200

    Add loading app configuration from file

--- python/prir/app.py
+++ python/prir/app.py
@@ -1,32 +1,33 @@
 from prir.config import app_logging
-from prir.files.mnist_file_manager import MnistFileManager
+from prir.files.mnist_trunc_file_manager import MnistTruncFileManager
 from prir.metrics import timer
 import prir.machinelearning.standarization.standarizator as standarizator
 import prir.machinelearning.classifier.knn as knn
+from prir.config.app_config import app_conf
 
 
 logger = app_logging.get_logger(__name__)
 logger.debug("APPLICATION START")
 
-dataset = MnistFileManager().read_dataset()
+dataset = MnistTruncFileManager().read_dataset(train_size=4500, test_size=1500)
 
 train_data_standarized_min_max = timer.call_and_measure(
         """standarizator.min_max_scaler(dataset.get('train_data'))""",
-        number=10)
+        number=app_conf['measures_count'])
 timer.call_and_measure(
         """knn.train_predict(train_data_standarized_min_max, dataset.get('train_target'), 
         dataset.get('test_data'), dataset.get('test_target'))""",
-        number=10)
+        number=app_conf['measures_count'])
 del train_data_standarized_min_max
 
 
 train_data_standarized_standard_scaler = timer.call_and_measure(
     """standarizator.standard_scaler(dataset.get('train_data'))""",
-    number=1)
+    number=app_conf['measures_count'])
 timer.call_and_measure(
         """knn.train_predict(train_data_standarized_standard_scaler, dataset.get('train_target'), 
         dataset.get('test_data'), dataset.get('test_target'))""",
-        number=1)
+        number=app_conf['measures_count'])
 del train_data_standarized_standard_scaler
 
 logger.debug("APPLICATION END")
--- python/prir/machinelearning/classifier/knn.py
+++ python/prir/machinelearning/classifier/knn.py
@@ -1,10 +1,12 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
 from prir.config import app_logging
+from prir.config.app_config import app_conf
 
 
 def train_predict(train_data, train_target, test_data, test_target):
-    model = KNeighborsClassifier(n_neighbors=1, algorithm='auto')
+    model = KNeighborsClassifier(n_neighbors=1, algorithm='brute', p=2, metric='minkowski',
+                                 n_jobs=app_conf['jobs_number'])
 
     # Train the model using the training set
     model.fit(train_data, train_target)
@@ -16,4 +18,4 @@ def train_predict(train_data, train_target, test_data, test_target):
     return predicted
 
 
-logger = app_logging.get_logger(__name__)
+logger = app_logging.get_logger('metrics')
